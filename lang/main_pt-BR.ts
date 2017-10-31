06a1d7a25a7fd514d32297f1ff2bf1c1d292cc09
Luca Bezerra <lucabezerra@gmail.com>
2017-10-30 21:58:39 -0300

    Added pt_BR translation.

---
+++ lang/main_pt-BR.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>StratumsphereTrayIcon</name>
+    <message>
+        <location filename="../main.cpp" line="82"/>
+        <source>Mostrar as &amp;notificações</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="87"/>
+        <location filename="../main.cpp" line="193"/>
+        <source>&amp;Status da atualização</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="91"/>
+        <source>&amp;Sair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="126"/>
+        <source>Atualizar…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="199"/>
+        <source>Este espaço está fechado</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="200"/>
+        <source>Bytespeicher fechou.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="203"/>
+        <source>Este espaço está aberto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="204"/>
+        <source>Bytespeicher abriu !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="207"/>
+        <source>Não podemos determinar o status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="215"/>
+        <source>Status desde : %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="218"/>
+        <source>Última atualização : %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
