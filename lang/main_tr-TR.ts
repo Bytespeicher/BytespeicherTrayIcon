ceeafb7ca2d83150d9982d9d362ce6faea4016ab
Berk Gureken <berkgureken@gmail.com>
2018-10-02 15:11:07 +0300

    Added Turkish translation.

---
+++ lang/main_tr-TR.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>StratumsphereTrayIcon</name>
+    <message>
+        <location filename="../main.cpp" line="83"/>
+        <source>Show &amp;notifications</source>
+        <translation>Bildirimleri &amp;göster</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="88"/>
+        <location filename="../main.cpp" line="194"/>
+        <source>&amp;Update status</source>
+        <translation>&amp;Durum güncelle</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="92"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Çıkış</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="127"/>
+        <source>Updating…</source>
+        <translation>Güncelleniyor…</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="200"/>
+        <source>Space is closed</source>
+        <translation>Alan kapalı</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="201"/>
+        <source>The Bytespeicher has just closed.</source>
+        <translation>Bytespeicher kapandı.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="204"/>
+        <source>Space is open</source>
+        <translation>Alan açık</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="205"/>
+        <source>The Bytespeicher has just opened!</source>
+        <translation>Bytespeicher açıldı!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="208"/>
+        <source>Could not determine opening status</source>
+        <translation>Açılış durumu belirlenemedi</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="216"/>
+        <source>Status since: %1</source>
+        <translation>Durum: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="219"/>
+        <source>Last update: %1</source>
+        <translation>Son güncelleme: %1</translation>
+    </message>
+</context>
+</TS>
