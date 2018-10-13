1cfd10ba53883111c73395ac7738129debc1e059
WangJihe <1321942045@qq.com>
2018-10-13 11:10:25 +0800

    Add Simplified Chinese (zh-cn)

---
+++ lang/main_zh-CN.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>StratumsphereTrayIcon</name>
+    <message>
+        <location filename="../main.cpp" line="83"/>
+        <source>Show &amp;notifications</source>
+        <translation>显示通知</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="88"/>
+        <location filename="../main.cpp" line="194"/>
+        <source>&amp;Update status</source>
+        <translation>&amp;更新状态</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="92"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;退出</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="127"/>
+        <source>Updating…</source>
+        <translation>更新中…</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="200"/>
+        <source>Space is closed</source>
+        <translation>空间已关闭</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="201"/>
+        <source>The Bytespeicher has just closed.</source>
+        <translation>Bytespeicher刚刚关闭.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="204"/>
+        <source>Space is open</source>
+        <translation>空间是开放的</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="205"/>
+        <source>The Bytespeicher has just opened!</source>
+        <translation>Bytespeicher刚刚打开!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="208"/>
+        <source>Could not determine opening status</source>
+        <translation>无法确定开放状态</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="216"/>
+        <source>Status since: %1</source>
+        <translation>状态来自于: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="219"/>
+        <source>Last update: %1</source>
+        <translation>最后更新: %1</translation>
+    </message>
+</context>
+</TS>
