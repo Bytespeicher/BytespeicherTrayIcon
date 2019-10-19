f9eb8b191d790a4937f75601bfed1fb4cbf40b76
argprogrammer <argprogrammer@gmail.com>
2019-10-19 14:09:49 +0330

    Add translation of persian language

---
+++ lang/main_fa-IR.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fa_IR">
+<context>
+    <name>StratumsphereTrayIcon</name>
+    <message>
+        <location filename="../main.cpp" line="83"/>
+        <source>Show &amp;notifications</source>
+        <translation>نمایش &amp;خبر</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="88"/>
+        <location filename="../main.cpp" line="194"/>
+        <source>&amp;Update status</source>
+        <translation>&amp;بروزرسانی وضعیت</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="92"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;خروج</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="127"/>
+        <source>Updating…</source>
+        <translation>در حال بروزرسانی...</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="200"/>
+        <source>Space is closed</source>
+        <translation>L&apos;فضا بسته شد</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="201"/>
+        <source>The Bytespeicher has just closed.</source>
+        <translation>نرم افزار&apos;بسته شد.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="204"/>
+        <source>Space is open</source>
+        <translation>&apos; فضا باز است</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="205"/>
+        <source>The Bytespeicher has just opened!</source>
+        <translation>نرم افزار&apos;باز شد!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="208"/>
+        <source>Could not determine opening status</source>
+        <translation>نمی تواند وضعیت باز بودن را تشخیص دهد</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="216"/>
+        <source>Status since: %1</source>
+        <translation>وضعیت از : %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="219"/>
+        <source>Last update: %1</source>
+        <translation>آخرین بروزرسانی : %1</translation>
+    </message>
+</context>
+</TS>
